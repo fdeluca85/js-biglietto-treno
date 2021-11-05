ecc0b3e482814d2b2c67a7c9bca7647bb5bf8751
francesca <fradeluca85@gmail.com>
2021-11-05 15:50:02 +0100

    prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // alert ("hello world")
+
+
+// richiesta Km
+let kilometri = parseFloat(prompt("Inserisci i Km da percorrere"));
+console.log(kilometri);
+
+
+// richiesta età
+
+let eta = parseInt(prompt("Inserisci la tua età"));
+console.log(eta);
