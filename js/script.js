8648f3a05f930351d2e77e1e25d1e9df16c99bed
francesca <fradeluca85@gmail.com>
2021-11-05 18:39:45 +0100

    prezzo finale html

--- js/script.js
+++ js/script.js
@@ -15,32 +15,37 @@ console.log(eta);
 // prezzo tratta
 
 let tratta = (kilometri * ".21");
-console.log(tratta);
+// console.log(tratta);
 
 
 
 // sconto minorenni del 20%
 let scontominorenni = (tratta * '20') / '100';
-console.log(scontominorenni);
-
-
+// console.log(scontominorenni);
 
 
 
 // sconto del 40% per i superiori di 65 anni
 let scontosenior = (tratta * '40') / '100';
-console.log(scontosenior);
+// console.log(scontosenior);
 
 
-// prezzo minorenni
+// prezzo finale
+let prezzo;
+            // prezzo minorenni
 if (eta < 18) {
-console.log (tratta - scontominorenni);
+    prezzo = (tratta - scontominorenni)
+// console.log (tratta - scontominorenni);
 } 
-
-// prezzo senior
+            // prezzo senior
 else if (eta >= 65) {
-    console.log (tratta - scontosenior);
+    prezzo = (tratta - scontosenior)
+    // console.log (tratta - scontosenior);
     }
 
+else {
+    prezzo = tratta
+    // console.log (tratta);
+    }
 
-
+    document.getElementById("prezzo").innerHTML = prezzo;
