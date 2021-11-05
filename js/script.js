d9f0a6953d4043de5fbe923e0535dab0ac80397e
francesca <fradeluca85@gmail.com>
2021-11-05 16:02:57 +0100

    prezzo tratta

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ console.log(kilometri);
 
 let eta = parseInt(prompt("Inserisci la tua età"));
 console.log(eta);
+
+
+// prezzo tratta
+
+let tratta = (kilometri * ".21")
+console.log(tratta);
+
+
+// sconto minorenni del 20%
+
+
+
+
+// sconto del 40% per i superiori di 65 anni
